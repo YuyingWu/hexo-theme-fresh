da8f5180d28d28ee30df07762f1294e7d9985f9f
wuyuying <wyy@wuyuyingdeMacBook-Pro.local>
2017-05-03 11:24:19 +0800

    build: dist files included

---
+++ source/build/js/aio.js
@@ -0,0 +1,44 @@
+'use strict';
+
+define('zoom', function (require, exports, module) {
+	var Zoom = {
+		version: '0.1.0'
+	};
+
+	module.exports = Zoom;
+});
+;'use strict';
+
+var Zoom = require('zoom');
+
+$(document).on('click', 'img', function () {
+	var self = $(this);
+
+	var OFFSET = 40;
+
+	var WINDOW = $(window);
+	var WIN_WIDTH = WINDOW.width();
+	var WIN_HEIGHT = WINDOW.height();
+
+	/*const imgElement = new Image();
+ imgElement.src = self.attr('src');*/
+	var NATURAL_WIDTH = this.naturalWidth;
+	var NATURAL_HEIGHT = this.naturalHeight;
+
+	var newWidth = Math.min(WIN_WIDTH, NATURAL_WIDTH);
+	var newHeight = Math.min(WIN_HEIGHT, NATURAL_HEIGHT);
+
+	$('body').css({
+		width: WIN_WIDTH,
+		height: WIN_HEIGHT,
+		overflow: 'hidden'
+	});
+
+	self.css({
+		position: 'absolute',
+		top: (WIN_HEIGHT - newHeight) / 2 + 'px',
+		left: (WIN_WIDTH - newWidth) / 2 + 'px',
+		width: WIN_WIDTH + 'px'
+		//height: newHeight + 'px'
+	});
+});
---
+++ source/build/js/modules.js
@@ -0,0 +1,9 @@
+'use strict';
+
+define('zoom', function (require, exports, module) {
+	var Zoom = {
+		version: '0.1.0'
+	};
+
+	module.exports = Zoom;
+});
